924fb98d0524e567a3110abd2c419b7b8a92671f
Samurai_Chia <jyjzone@163.com>
2024-12-07 16:01:26 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        let  sample = "this is sample";
+        println!("****the value of sample:{sample}***");
+    }
+    //下面的語句無法執行。因為sample已經離開它自己的作用域，已經被內存回收了
+    //println!("****the value of sample:{sample}***");
 }
